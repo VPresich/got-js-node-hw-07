32e5628ad5f412ffc476bb9112b07dca7a267ba6
VPresich <valya.alexandrova.presich@gmail.com>
2023-11-03 03:46:44 +0100

    Update photos.js

--- src/js/data/photos.js
+++ src/js/data/photos.js
@@ -2,27 +2,27 @@
 
 const images = [
     {
-      url: "../img/photo-1-1.jpg",
+      url: "/img/photo-1-1.jpg",
       alt: "photo-1-1",
     },
     {
-      url: "../img/photo-1-2.jpg",
+      url: "/img/photo-1-2.jpg",
       alt: "photo-1-2",
     },
     {
-      url: "../img/photo-1-3.jpg",
+      url: "/img/photo-1-3.jpg",
       alt: "photo-1-3",
     },
     {
-      url: "../img/photo-2-1.jpg",
+      url: "/img/photo-2-1.jpg",
       alt: "photo-2-1",
     },
     {
-      url: "../img/photo-2-2.jpg",
+      url: "/img/photo-2-2.jpg",
       alt: "photo-2-2",
     },
     {
-      url: "../img/photo-2-3.jpg",
+      url: "/img/photo-2-3.jpg",
       alt: "photo-2-3",
     },
   ];
