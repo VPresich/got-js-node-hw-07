1d46f13c3accf0359b746cdcf3ffab6726e618e1
VPresich <111353667+VPresich@users.noreply.github.com>
2023-11-05 07:56:10 +0000

    Deploying to gh-pages from @ VPresich/got-js-node-hw-07@719d1ca6628b4ffee7996f45bdbb05c1e3eee430 🚀

--- assets/task-7-1-548ccb06.js
+++
@@ -1,1 +0,0 @@
-import"./styles-218a2ab6.js";const c=document.querySelector("#categories"),t=c.querySelectorAll(".item");console.log(`Number of categories: ${t.length}`);t.forEach(e=>{const o=e.firstElementChild.textContent,l=e.lastElementChild.children;console.log(`Category: ${o}`),console.log(`Elements: ${l.length}`)});
---
+++ assets/task-7-3-5ac6096f.js
@@ -0,0 +1,1 @@
+import"./styles-8109241a.js";const u=document.getElementById("name-input");u.addEventListener("input",o);function o(n){const t=n.currentTarget.value.trim(),e=document.getElementById("name-output");e.textContent=t!==""?t:"Anonymous"}
